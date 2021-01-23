1015d0db9926016367e20ffe0683870520804ee3
Shunsuke-Hara <shunsuke.hara02@gmail.com>
2021-01-23 23:02:02 +0900

    コミットテスト

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
+  
 /**
  * このページへアクセスされたとき以下の挙動を行う
- * 
+ *
  * 1. ローカルストレージに格納されている値を取得する
  * 2. 手順1で取得した値をtextareaにセットする
  */
+// ページ読み込み完了後にonInitファンクションを実行
+window.onload = onInit()
+
+function onInit() {
+    // ローカルストレージの値を取得
+    let playerName = localStorage.getItem(srageKey)
+    // テキストエリアにローカルストレージの値を設定
+    members.value = playerName
+}
+
 
 /**
  * ボタン押下時以下の挙動を行う
- * 
+ *
  * 1. textareaに入力されている値を取得する
  * 2. 手順1で取得した値をローカルストレージに格納する
  * 3. share.htmlへ遷移する
  */
+// 次へボタンクリック後にonClickファンクションを実行
+document.getElementById("done").onclick = moveShare()
+
+function moveShare() {
+    // キー生成
+    const playedData = 'before'
+
+    // テキストエリアの値を取得
+    let beforeMember = members.getItem(strageKey)
+
+    // テキストエリアの値をローカルストレージに格納
+    localStorage.setItem(playedData, beforeMember)
+
+    // ページ遷移
+    window.location.href = 'share.html'
+}
--- js/role.js
+++ js/role.js
@@ -5,3 +5,9 @@
  * 2. 手順1で取得した値を復号化する
  * 3. 画面に名前・役職（・お題）を表示させる
  */
+window.onload = onInit()
+
+function onInit() {
+    let params = window.location.search
+    
+}
